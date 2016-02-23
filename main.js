6c60e6fc30cf73c435ce977928afac82556c3a86
mcbrooks95 <mcbrooks95@gmail.com>
2016-02-23 14:34:22 -0500

    adding json stuff

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
-
-	$('div').click(function() {
-		$('div').fadeOut();	
-	});
-
-
+    
+      $.getJSON("http://ip-api.com/json", function(json) {
+        $(".message").html(JSON.stringify(json));
+        var city = json["city"];
+        var region = json["region"];
+        var key = "6b02fb2a4f4e51e0";
+        var url = "http://api.wunderground.com/api/" + key + "/conditions/q/" + region + "/" + city + ".json";
+        $.getJSON(url, function(json) {
+          $(".message").html(JSON.stringify(json.current_observation.temperature_string));
+        });
+        //$(".message").html(url);
+        
+      });
 });
