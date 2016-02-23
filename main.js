88db9799fcfaa565fb6a84de1eb4dc7b5aad7ab8
mcbrooks95 <mcbrooks95@gmail.com>
2016-02-23 14:39:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     
       $.getJSON("http://ip-api.com/json", function(json) {
         $(".message").html(JSON.stringify(json));
-        var city = json["city"];
+        /*var city = json["city"];
         var region = json["region"];
         var key = "6b02fb2a4f4e51e0";
         var url = "http://api.wunderground.com/api/" + key + "/conditions/q/" + region + "/" + city + ".json";
@@ -10,6 +10,6 @@ $(document).ready(function() {
           $(".message").html(JSON.stringify(json.current_observation.temperature_string));
         });
         //$(".message").html(url);
-        
+        */
       });
 });
