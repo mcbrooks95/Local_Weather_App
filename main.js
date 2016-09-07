d3754fdc6d899ba2bb9ec78f1c137c3d7ff28bdc
Mark Brooks <brooks80@purdue.edu>
2016-09-06 22:48:35 -0400

    indenting fixes

--- main.js
+++ main.js
@@ -4,32 +4,28 @@ $(document).ready(function() {
     var CorF;
    
     $.getJSON("http://ip-api.com/json", function(json) {
-		  key = "6b02fb2a4f4e51e0";
-      url = "http://api.wunderground.com/api/" + key + "/conditions/q/" + JSON.stringify(json["region"]).replace(/\"/g, "") + "/" + JSON.stringify(json["city"]).replace(/\"/g, "") + ".json";
-      $(".text2").html(JSON.stringify(json["city"]).replace(/\"/g, "") + ", " + JSON.stringify(json["region"]).replace(/\"/g, "") + ", " + JSON.stringify(json["countryCode"]).replace(/\"/g, ""));
+    	key = "6b02fb2a4f4e51e0";
+        url = "http://api.wunderground.com/api/" + key + "/conditions/q/" + JSON.stringify(json["region"]).replace(/\"/g, "") + "/" + JSON.stringify(json["city"]).replace(/\"/g, "") + ".json";
+        $(".text2").html(JSON.stringify(json["city"]).replace(/\"/g, "") + ", " + JSON.stringify(json["region"]).replace(/\"/g, "") + ", " + JSON.stringify(json["countryCode"]).replace(/\"/g, ""));
 
-      $.getJSON(url, function(json) {
-        $(".text3").html(JSON.stringify(json.current_observation.temp_f));  
-			  $("img").attr("src", JSON.stringify(json.current_observation.icon_url).replace(/\"/g, ""));     
-        $(".text4").html(JSON.stringify(json.current_observation.weather).replace(/\"/g, ""));
-      });
-    });
-
-
-    $('.asText').click(function() {
-        
-        CorF = CorF === "C" ? "F" : "C";
-               // $(".text4").html(url);
         $.getJSON(url, function(json) {
-			if(CorF === "C") {
-			    $(".text3").html(JSON.stringify(json.current_observation.temp_c)); 
-				$(".asText").html("C");
-			}
-			else {
-				$(".text3").html(JSON.stringify(json.current_observation.temp_f));
-				$(".asText").html("F");
-			}	          
+            $(".text3").html(JSON.stringify(json.current_observation.temp_f));  
+	    $("img").attr("src", JSON.stringify(json.current_observation.icon_url).replace(/\"/g, ""));     
+            $(".text4").html(JSON.stringify(json.current_observation.weather).replace(/\"/g, ""));
         });
     });
 
+    $('.asText').click(function() {
+        CorF = CorF === "C" ? "F" : "C";
+        $.getJSON(url, function(json) {
+	    if(CorF === "C") {
+	        $(".text3").html(JSON.stringify(json.current_observation.temp_c)); 
+		$(".asText").html("C");
+	    }
+	    else {
+	        $(".text3").html(JSON.stringify(json.current_observation.temp_f));
+		$(".asText").html("F");
+	    }	          
+        });
+    });
 });
